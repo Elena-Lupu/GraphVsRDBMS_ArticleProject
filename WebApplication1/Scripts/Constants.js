346347e17a4add42953f21fcab3d1ebaef319912
Elena-Lupu <elena.lupu@stud.acs.upb.ro>
2024-04-30 04:28:35 +0300

    Add project files.

---
+++ WebApplication1/Scripts/Constants.js
@@ -0,0 +1,45 @@
+﻿export var etajData = [
+    {
+        "nume": "Parter",
+        "id": "0"
+    },
+    {
+        "nume": "Etaj 1",
+        "id": "1"
+    },
+    {
+        "nume": "Etaj 2",
+        "id": "2"
+    },
+    {
+        "nume": "Etaj 3",
+        "id": "3"
+    },
+    {
+        "nume": "Etaj 4",
+        "id": "4"
+    },
+    {
+        "nume": "Etaj 5",
+        "id": "5"
+    },
+    {
+        "nume": "Etaj 6",
+        "id": "6"
+    },
+    {
+        "nume": "Etaj 7",
+        "id": "7"
+    }
+];
+
+export var BdData = [
+    {
+        "nume": "Neo4j",
+        "id": "1"
+    },
+    {
+        "nume": "MSSQL Server",
+        "id": "2"
+    }
+];
---
+++ WebApplication1/Scripts/ReactComps.jsx
@@ -0,0 +1,89 @@
+﻿import * as JSONdata from "./Constants.js"
+
+class Text extends React.Component {
+    render() {
+        return (
+            <div className={this.props.marime}>
+                <span>{this.props.text}</span>
+            </div>
+        );
+    }
+}
+
+class DropDown extends React.Component {
+    render() {
+        return (
+            <div style={{ width: this.props.width, display: "flex" }}>
+                <Text marime="TextSelect" text={this.props.text} />
+                <select className="selectorStyle">{this.props.data.map((item) => { return (<option value={item.id}>{item.nume}</option>); })}</select>
+            </div>
+        );
+    }
+}
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div style={{ width: "40%" }}><input className="selectorStyle" placeholder="Cauta..." style={{ width: "50%" }} /></div>
+        );
+    }
+}
+
+class CheckBox extends React.Component {
+    render() {
+        return (
+            <div className="CheckBoxStyle">
+                <input type="checkbox" />
+                <Text marime="TextSelect" text={this.props.text} />
+            </div>
+        );
+    }
+}
+
+class Meniu extends React.Component {
+    render() {
+        return (
+            <div className="MeniuStyle">
+                <div style={{ width: "15%" }}><Text marime="TextTitlu" text="Precis Way" /></div>
+                <DropDown text="Etaj" width="20%" data={JSONdata.etajData} />
+                <SearchBar />
+                <DropDown text="Baza de date utilizata" width="30%" data={JSONdata.BdData} />
+            </div>
+        );
+    }
+}
+
+class SideBar extends React.Component {
+    render() {
+        return (
+            <div className="BarStyle SideBarStyle">
+                <Text text="Configurarea traseului" marime="TextSubtitlu"/>
+                <DropDown text="Punct plecare" width="90%" data={JSONdata.etajData} />
+                <DropDown text="Punct destinatie" width="90%" data={JSONdata.etajData} />
+                <Text text="Filtre" marime="TextSubtitlu" />
+                <CheckBox text="UnFiltru_1"/>
+                <CheckBox text="UnFiltru_2"/>
+            </div>
+        );
+    }
+}
+
+class DetailBar extends React.Component {
+    render() {
+        return (
+            <div className="BarStyle DetailBarStyle">
+                <Text text="Detalii traseu" marime="TextSubtitlu" />
+            </div>
+        );
+    }
+}
+
+class Pagina extends React.Component {
+    render() {
+        return (
+            <><SideBar /><DetailBar /><Meniu /></>
+        );
+    }
+}
+
+ReactDOM.render(<Pagina />, document.getElementById('unDiv'));
